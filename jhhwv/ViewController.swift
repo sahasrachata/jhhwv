0fda18458279f6d650c4407221dfd5b5b9f1fa8d
Sahasra Chatakondu <sahasra.chatakondu@gmail.com>
2022-08-02 13:02:49 -0500

    Initial Commit

---
+++ jhhwv/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  jhhwv
+//
+//  Created by scholar on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var question: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitTapped(_ sender: UIButton) {
+    }
+    
+}
+
